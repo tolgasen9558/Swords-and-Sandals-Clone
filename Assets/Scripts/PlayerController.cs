2e1efc744c4a31eadf3ca0f346a3a780a5079ee9
Tolga Şen <tolgasen9558@gmail.com>
2020-03-04 00:22:04 +0100

    Create basic scene with player and enemy, add player left right movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float stepSize = 0.75f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * stepSize);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * stepSize);
+        }
+    }
+}
